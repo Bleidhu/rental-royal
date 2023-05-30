706bb2aa209f79ec35a6d2b3e95382fd256dd632
Bleidhu <ichigosr2014@gmail.com>
2023-05-30 19:21:44 +0200

    removed copied code, starting from scratch

--- index.js
+++ index.js
@@ -1,29 +1,12 @@
-const dotenv = require('dotenv');
-dotenv.config({ path: './.env' });
-
-const path = require('path');
-
-const helmet = require('helmet');
-const multer = require('multer');
-
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-let session = require('express-session');
-const csrf = requier('csurf');
-const compression = require('compression');
-const morgan = reuire('morgan');
-const publicDir = path.join(__dirname, './public');
-const routes = require('./src/routes/routes.js');
+const dotenv = require('dotenv').config();
+const express = require('express')
 
 const app = express();
 
+const auth = require('./routes/auth.js');
 
+app.use(auth);
 
-app.use('/', routes);
-
-let port = process.env.PORT;
-
-app.listen(port || 3000, () => {
-    console.log(`Listening on port ${port || 3000}`);
+app.get('/', (req, res) => {
+    res.send('Hi');
 })
--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,5 +1,29 @@
 const User = require('../models/user');
 const { validationResult } = require("express-validator");
+const jwt = require('jsonwebtoken')
+const redis = require('redis')
+
+
+const handleSignIn = (db, bcrypt, req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return Promise.reject('incorrect form submission');
+    }
+
+    return User.find({ email: email }, function (err, user) {
+        if (err) {
+            return Promise.reject('wrong credentials');
+        }
+        const userValid = true; 0
+        if (userValid) {
+            return User.findOne({ email: email })
+        }
+    }).catch(err => Promise.reject('wrong credentials'));
+
+    return
+}
+
 
 exports.signup = (req, res, next) => {
     const errors = validationResult(req);
@@ -9,4 +33,6 @@ exports.signup = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
     const name = req.body.name;
+
+
 }
